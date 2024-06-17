49ec1e9220ac8a2ab90ddc3fdb0cd9099294f946
Hurley <hurleywflow@gmail.com>
2024-06-17 22:56:09 +1000

    feat: added animation fulse for NetCode footer button

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 /* eslint-disable react/react-in-jsx-scope */
 const Footer = (): JSX.Element => {
 	const year = new Date().getFullYear();
 	return (
-		<footer className="p-0 relative m-0 w-full bg-slate-200 pt-2 text-center text-slate-950 dark:bg-foreground dark:text-slate-100">
+		<footer className='p-0 relative m-0 w-full bg-slate-200 pt-2 text-center text-slate-950 dark:bg-foreground dark:text-slate-100'>
 			{/* <div className='absolute inset-0 bg-[radial-gradient(40%_128px_at_50%_0%,theme(backgroundColor.white/10%),transparent)]' />
 
 			<div className='absolute size-full bg-[url(https://lunarui.dev/twitter/noise.png)] bg-[size:96px] bg-repeat opacity-[0.025]' />
@@ -21,14 +21,14 @@ const Footer = (): JSX.Element => {
 					</div>
 				</div>
 			</div> */}
-			<small className="mb-2 block text-xs">
+			<small className='mb-2 block text-xs'>
 				Copyright &copy; {year} Villa Dalat TrungNghia. All rights reserved.
 			</small>
-			<p className="mb-3 text-xs">
-				<span className="font-semibold">About this website:</span> Designed and
+			<p className='mb-3 text-xs'>
+				<span className='font-semibold'>About this website:</span> Designed and
 				built by
-				<Link href="https://netcodedev.com" target="_blank">
-					<Button variant="ghost" className="m-1 h-4 p-0">
+				<Link href='https://netcodedev.com' target='_blank'>
+					<Button variant='ghost' className='m-1 h-4 p-0 animate-pulse'>
 						NetCode
 					</Button>
 				</Link>
